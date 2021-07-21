5085484d757f34be72b99d9012e8563842e8e9a8
KappaJ <masonticehurst@me.com>
2021-07-20 21:54:39 -0400

    Update inline hidden display to Bootstrap d-none, chart gen

--- index.php
+++ index.php
@@ -40,6 +40,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery.gridstrap@0.0.0-semantically-released/dist/jquery.gridstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/flip.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+
 	<!-- Javascript -->
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
@@ -49,77 +50,68 @@
 	<script src="js/flip.min.js"></script>
 	<script src="http://code.jquery.com/color/jquery.color-2.1.2.min.js"></script>
 	<script src="https://unpkg.com/gelerator@4.0.0/dist/gelerator.min.js"></script>
+
 	<!-- Page Tags -->
 	<title></title>
 </head>
 <body>
 
+<?php
+	require( 'userConfig.php' );
+	$config = new \Configuration();
+?>
+
+<!-- MirrorFramework :: Base -->
 <div class="container-fluid px-2 py-2">
+		<!-- MirrorFramework :: Top.Row -->
 		<div class="row">
+			<!-- MirrorFramework :: Top.Left.Column -->
 			<div class="col-3">
-				<div class="text-center tlLoading d-flex align-items-center justify-content-center" style="width: 600px;height: 400px;background-color: #101010;border-radius: 4px;vertical-align: middle;">
-					<div class="spinner-border tlSpinner" style="width: 5vw; height: 5vw;" role="status"></div>
+				<!-- MirrorFramework :: Loading -->
+				<div class="text-center tlLoading d-flex align-items-center justify-content-center">
+					<div class="spinner-border tlSpinner" role="status"></div>
 				</div>
-				<!--
-				<div class="weatherBG">
-					<div class="container px-4 py-2">
-						<div class="row">
-							<div class="col-8">
-								<h1 class="display-6 sunnyText">Party Cloudy</h1>
-								<div class="temp">
-									<span class="sunnyTemp">24</span>
-									<span class="deg">°F</span>
-								</div>
-								<div class="location">Salt Lake City, Utah</div>
-							</div>
-							<div class="col-4">
-								<div style="display: flex;justify-content: center;align-items: center;width: 100%;height: 100%;">
-									<img class="weatherIcon" src="img/icon_few_clouds_day.svg"/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				-->
-				<div class="row weatherDisplay" style="display: none;">
+
+				<!-- MirrorFramework :: Weather -->
+				<div class="row weatherDisplay d-none">
 					<div class="col-6 m-auto">
 						<div>
-							<div class="row" style="line-height: 1;">
-								<div class="col-6" style="text-align: right;">
+							<div class="row lh-1">
+								<div class="col-6 text-end">
 									<img class="weatherIcon" id="currentWeatherIcon" src="img/icon_few_clouds_day.svg"/>
 								</div>
 								<div class="col-6 d-flex text-left align-items-center">
-									<span class="sunnyTemp"></span>
+									<span class="temperature"></span>
 								</div>
 							</div>
 						</div>
-						<div class="row" style="line-height: 1;">
+						<div class="row lh-1">
 							<div class="col-12 text-center">
-								<span class="text-center" style="font-weight: 100;font-size: 2em;" id="cityName"></span>
+								<span class="text-center fs-3 fw-light cityName"></span>
 							</div>
 						</div>
 					</div>
 					<div class="col-6 d-flex flex-column justify-content-between">
-						<div style="font-size: 2.5em;font-weight: 200;white-space: nowrap;" class="weatherDesc"></div>
+						<div class="weatherDesc fw-lighter text-nowrap fs-1"></div>
 
 						<div>
-							<div style="font-size: 0.8vw;font-weight: 400;line-height: 1;padding-top: 1ch;">
+							<div class="fw-light lh-1" style="font-size: 0.8vw;padding-top: 1ch;">
 								<div class="row">
 									<div class="col-1">
 										<img class="miniIcon" src="img/icon_clear_sky_day.svg"/>
 									</div>
 									<div class="col-5">
-										<span class="fw-400" id="sunRise"></span>
+										<span class="fw-400 sunRise"></span>
 									</div>
 									<div class="col-1">
 										<img class="miniIcon" src="img/icon_clear_sky_night.svg"/>
 									</div>
 									<div class="col-5">
-										<span class="fw-400" id="sunSet"></span>
+										<span class="fw-400 sunSet"></span>
 									</div>
 								</div>
 							</div>
-							<div style="font-size: 0.8vw;font-weight: 400;line-height: 1;">
+							<div class="fw-light lh-1" style="font-size: 0.8vw;">
 								<div class="row">
 									<div class="col-6">
 										<span class="fw-200">Humidity</span>
@@ -129,7 +121,7 @@
 									</div>
 								</div>
 							</div>
-							<div style="font-size: 0.8vw;font-weight: 400;line-height: 1;">
+							<div class="fw-light lh-1" style="font-size: 0.8vw;">
 								<div class="row">
 									<div class="col-6">
 										<span class="fw-200">Wind</span>
@@ -143,7 +135,7 @@
 					</div>
 				</div>
 				<br>
-				<div class="container" id="weatherForecast" style="display: none;">
+				<div class="container d-none" id="weatherForecast">
 					<?php
 						for($i = 1; $i < 8; $i++ ){
 							echo '<div class="row weatherBar" id="weather-day-' . $i . '">';
@@ -166,14 +158,19 @@
 					?>
 				</div>
 			</div>
+			<!-- MirrorFramework :: Spacer -->
 			<div class="col-3">
 			</div>
-			<div class="col-6">
+			<!-- MirrorFramework :: Top.Right.Column -->
+			<div class="col-6 col-auto">
 				<div class="container justify-end">
-					<div class="text-center trLoading d-flex align-items-center justify-content-center" style="width: 400px;height: 200px;background-color: #101010;border-radius: 4px;vertical-align: middle;">
+					<!-- MirrorFramework :: Loading -->
+					<div class="text-center trLoading d-flex align-items-center justify-content-center align-middle" style="width: 400px;height: 200px;background-color: #101010;border-radius: 4px;">
 						<div class="spinner-border trSpinner" style="width: 5vw; height: 5vw;" role="status"></div>
 					</div>
-					<div class="display-1" style="display: none;">
+
+					<!-- MirrorFramework :: DateTime -->
+					<div class="display-1 d-none">
 						<div class="time"></div>
 						<div class="date"></div>
 					</div>
@@ -186,98 +183,46 @@
 	<div class="text-center blLoading d-flex align-items-center justify-content-center" style="width: 800px;height: 120px;background-color: #101010;border-radius: 4px;vertical-align: middle;">
 		<div class="spinner-border blSpinner" style="width: 2vw; height: 2vw;" role="status"></div>
 	</div>
-	<div class="row" id="Stocks" style="overflow: hidden;white-space: nowrap;display: none;">
-		<div class="col-2 pe-0">
-			<div class="row">
-				<div class="col-7">
-					<div id="chart"></div>
-				</div>
-				<div class="col-5">
-					<br>
-					<div class="stockContainer">
-						<div class="d-flex align-items-center">
-							<svg width="16" height="16">
-								<circle fill="#ff0000" stroke="none" cx="8" cy="8" r="8">
-									<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/>
-								</circle>
-							</svg>
-							<span class="stockName stockName0 fw-500"></span>
-						</div>
-						<div class="stockFullName stockFullName0 fw-100"></div>
-						<div class="stockPrice">
-							<div class="fs-2 fw-700">$</div>
-							<div class="flipPrice flipPrice0 fs-2 fw-700"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Stock 2 -->
-		<div class="col-2 pe-0">
-			<div class="row">
-				<div class="col-6">
-					<div id="chart2"></div>
-				</div>
-				<div class="col-6">
-					<br>
-					<div class="stockContainer">
-						<div class="d-flex align-items-center">
-							<svg width="16" height="16">
-								<circle fill="#ff0000" stroke="none" cx="8" cy="8" r="8">
-									<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/>
-								</circle>
-							</svg>
-							<span class="stockName stockName1 fw-500"></span>
-						</div>
-						<div class="stockFullName stockFullName1 fw-100"></div>
-						<div class="stockPrice">
-							<div class="fs-2 fw-700">$</div>
-							<div class="flipPrice flipPrice1 fs-2 fw-700"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Stock 3 -->
-		<div class="col-2 pe-0">
-			<div class="row">
-				<div class="col-6">
-					<div id="chart3"></div>
-				</div>
-				<div class="col-6">
-					<br>
-					<div class="stockContainer">
-						<div class="d-flex align-items-center">
-							<svg width="16" height="16">
-								<circle fill="#ff0000" stroke="none" cx="8" cy="8" r="8">
-									<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/>
-								</circle>
-							</svg>
-							<span class="stockName stockName2 fw-500"></span>
-						</div>
-						<div class="stockFullName stockFullName2 fw-100"></div>
-						<div class="stockPrice">
-							<div class="fs-2 fw-700">$</div>
-							<div class="flipPrice flipPrice2 fs-2 fw-700"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="row text-nowrap overflow-hidden d-none stocks">
+		<?php
+			$numStocks = count( $config->getTrackedStocks() );
+			for( $i = 0; $i < $numStocks; $i++ ){
+				echo '<div class="col-2 pe-0">';
+					echo '<div class="row">';
+						echo '<div class="col-7">';
+							echo '<div id="chart' . $i . '"></div>';
+						echo '</div>';
+						echo '<div class="col-5">';
+							echo '<br>';
+							echo '<div class="stockContainer">';
+								echo '<div class="d-flex align-items-center">';
+									echo '<svg width="16" height="16">';
+										echo '<circle fill="#ff0000" stroke="none" cx="8" cy="8" r="8">';
+											echo '<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/>';
+										echo '</circle>';
+									echo '</svg>';
+									echo '<span class="stockName stockName' . $i . ' fw-500"></span>';
+								echo '</div>';
+								echo '<div class="stockFullName stockFullName' . $i . ' fw-100"></div>';
+								echo '<div class="stockPrice align-items-center">';
+									echo '<div class="fs-2 fw-700">$</div>';
+									echo '<div class="flipPrice flipPrice0 fs-2 fw-700"></div>';
+								echo '</div>';
+							echo '</div>';
+						echo '</div>';
+					echo '</div>';
+				echo '</div>';
+			}
+		?>
 	</div>
 </div>
 
 <script src="modules/stock.js"></script>
 <script src="modules/datetime.js"></script>
 <script src="modules/weather.js"></script>
-
+<script src="modules/rss.js"></script>
 <!-- Debug Only! -->
-<script type="text/javascript">
-	var c1 = genChartOptions([1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]);
-	renderChart( "#chart", c1 );
-</script>
+<script type="text/javascript"></script>
 
 </body>
 </html>
